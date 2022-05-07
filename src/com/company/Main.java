c0bc17757fed458fd204071654e1c2e22eec701d
Abdumalik07 <104436538+Abdumalik07@users.noreply.github.com>
2022-05-08 00:37:12 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class class1=new Class(12,"Hello",new int[]{1,2,4,6,7,4,8});
+
+        System.out.println("Number: "+class1.getNumber());
+        System.out.println("Word: "+class1.getWord());
+        System.out.println("Array: "+Arrays.toString(class1.getArray()));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
